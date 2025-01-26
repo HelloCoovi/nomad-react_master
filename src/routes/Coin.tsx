1c36d9be5d3107fcdd0dde4e7bfb94612074c822
Coovi <hellocoovi@gmail.com>
2025-01-27 00:22:56 +0900

    🎯💄✨ react-router의 State 기능으로 데이터 전달 받기 + 스타일 추가

--- src/routes/Coin.tsx
+++ src/routes/Coin.tsx
@@ -1,12 +1,65 @@
-import { useParams } from "react-router";
+import styled from "styled-components";
+import { useState } from "react";
+import { useParams, useLocation } from "react-router";
 
 export default function Coin() {
   const params = useParams();
-  console.log(params.coinId);
+  const location = useLocation();
+
+  const [isLoading, setIsLoading] = useState(true);
 
   return (
-    <div>
-      <h2>Coin ID: {params.coinId}</h2>
-    </div>
+    <S.Container>
+      <S.Header>
+        <S.Title>{location?.state?.coinName || "Loading..."}</S.Title>
+      </S.Header>
+      {isLoading ? <S.Loader>Loading...</S.Loader> : null}
+    </S.Container>
   );
 }
+
+const S = {
+  Header: styled.header`
+    color: ${(props) => props.theme.accentColor};
+  `,
+
+  Title: styled.h1`
+    font-size: 48px;
+    color: ${(props) => props.theme.accentColor};
+  `,
+
+  Container: styled.div`
+    padding: 0px 20px;
+    max-width: 480px;
+    margin: 0 auto;
+  `,
+
+  CoinsList: styled.ul``,
+  Coin: styled.li`
+    background-color: white;
+    color: ${(props) => props.theme.bgColor};
+    border-radius: 15px;
+    margin-bottom: 10px;
+    a {
+      display: flex;
+      align-items: center;
+      padding: 20px;
+      transition: color 0.2s ease-in;
+    }
+    &:hover {
+      a {
+        color: ${(props) => props.theme.accentColor};
+      }
+    }
+  `,
+  CoinImg: styled.img`
+    width: 35px;
+    height: 35px;
+    margin-right: 10px;
+  `,
+
+  Loader: styled.span`
+    text-align: center;
+    display: block;
+  `,
+};
