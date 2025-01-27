44cb3991699c74819335c667ec00b2ffde26294a
Coovi <hellocoovi@gmail.com>
2025-01-27 18:37:11 +0900

    ✨ `useMatch`를 사용해서 중첩 라우트 객체 받게 설정 - 경로에 따라 다른 ui 노출되게 설정 + ui 수정

--- src/routes/Coin.tsx
+++ src/routes/Coin.tsx
@@ -2,12 +2,16 @@ import styled from "styled-components";
 import { useState } from "react";
 import { useParams, useLocation } from "react-router";
 
-import { Outlet } from "react-router-dom";
+import { Outlet, Link, useMatch } from "react-router-dom";
+
 export default function Coin() {
   const params = useParams();
   const location = useLocation();
 
   const [isLoading, setIsLoading] = useState(true);
+
+  const priceMatch = useMatch("/:coinId/price");
+  const chartMatch = useMatch("/:coinId/chart");
 
   console.log(
     `\n┏━━━ 💡 💡 params 💡 💡 ━━━\n`,
@@ -21,6 +25,15 @@ export default function Coin() {
         <S.Title>{location?.state?.coinName || "Loading..."}</S.Title>
       </S.Header>
       {isLoading ? <S.Loader>Loading...</S.Loader> : null}
+
+      <S.Tabs>
+        <S.Tab $isActive={chartMatch !== null}>
+          <Link to={`/${params.coinId}/chart`}>Chart</Link>
+        </S.Tab>
+        <S.Tab $isActive={priceMatch !== null}>
+          <Link to={`/${params.coinId}/price`}>Price</Link>
+        </S.Tab>
+      </S.Tabs>
 
       <Outlet />
     </S.Container>
@@ -65,6 +78,27 @@ const S = {
     width: 35px;
     height: 35px;
     margin-right: 10px;
+  `,
+
+  Tabs: styled.div`
+    display: grid;
+    grid-template-columns: repeat(2, 1fr);
+    margin: 25px 0px;
+    gap: 10px;
+  `,
+  Tab: styled.span<{ $isActive: boolean }>`
+    text-align: center;
+    text-transform: uppercase;
+    font-size: 12px;
+    font-weight: 400;
+    background-color: rgba(0, 0, 0, 0.5);
+    padding: 7px 0px;
+    border-radius: 10px;
+    color: ${(props) =>
+      props.$isActive ? props.theme.accentColor : props.theme.textColor};
+    a {
+      display: block;
+    }
   `,
 
   Loader: styled.span`
